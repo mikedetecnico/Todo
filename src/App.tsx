a408b64eacfb87e078bf845034401e0b55314268
Michael Graessle <michael.graessle@gmail.com>
2023-11-08 21:14:16 -0800

    using bootstrap for signup and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,66 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navigationbar from './components/NavigationBar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
+    <div className="App">
+        <Router>
+            <Navigationbar />
+            <div className="auth-wrapper">
+              <div className="auth-inner">
+                <Routes>
+                  <Route path='/' element={<Login /> }/>
+                  <Route path='/login' element={<Login /> }/>
+                  <Route path='/signup' element={<Signup />}/>
+                </Routes>
+              </div>
+            </div>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
+/*
+import {NavLink, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+
+function App() {
+  return (
     <Router>
-      <section className='navbar-section'>
-        <nav className='main-navbar'>
-          <div className='container'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <Link className='navlink' to={'/login'}>Login</Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='navlink' to={'/signup'}>Signup</Link>
-              </li>
-            </ul>
+      <div className='App'>
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+              <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                  <Nav>
+                      <NavLink to="/">Home</NavLink>
+                      <NavLink to="/login">Login</NavLink>
+                      <NavLink to="/signup">Sign Up</NavLink>
+                  </Nav>
+              </Navbar.Collapse>     
+          </Navbar>
+        {
+        <section className='auth-section'>
+          <div className='auth-wrapper'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
           </div>
-        </nav>
-      </section>
-      <section className='auth-section'>
-        <div className='auth-wrapper'>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </section>
+        </section>
+        }
+      </div>
     </Router>
   )
 }
 
 export default App
+*/

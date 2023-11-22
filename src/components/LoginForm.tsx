777e2497b0a7ae29e7ad5a94cfd223d51cdc602a
Michael Graessle <michael.graessle@gmail.com>
2023-11-21 16:25:58 -0800

    adding new top header to login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import MainTopHeader from "./MainTopHeader";
 
 interface LoginFormProps {
     buttonText: string;
@@ -17,42 +18,45 @@ const LoginForm = ({buttonText, onSubmitCallback, route, routeText}: LoginFormPr
     }
 
     return (
-        <div className='h-screen flex items-center justify-center bg-primary'>
-            <form onSubmit={handleSubmit} className='bg-primarygray shadow-md rounded px-8 pt-6 pb-8 mb-4 w-3/4'>
-                <div className='mb-4'>
-                    <label className='block text-white text-sm font-bold mb-2'>
-                    Email
-                    </label>
-                    <input className='shadow appearance-none border rounded w-full py-2 px-3 text-primarygray leading-tight focus:outline-none focus:shadow-outline' 
-                        id='email' 
-                        type='text' 
-                        placeholder='Email' 
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className='mb-4'>
-                    <label className='block text-white text-sm font-bold mb-2'>
-                    Password
-                    </label>
-                    <input className='shadow appearance-none border rounded w-full py-2 px-3 text-primarygray mb-3 leading-tight focus:outline-none focus:shadow-outline' 
-                        id='password' 
-                        type='password' 
-                        placeholder='password' 
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className='md:flex md:items-center'>
-                    <div className='md:w-2/3'>
-                        <button className='shadow bg-primaryblue hover:bg-hoverblue focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded' type='submit'>
-                            {buttonText}
-                        </button>
-                        <a className='inline-block align-baseline font-bold text-sm text-primaryblue hover:text-hoverblue px-4' href={route}>
-                            {routeText}
-                        </a>
+        <>
+            <MainTopHeader />
+            <div className='h-screen flex items-center justify-center bg-primary'>
+                <form onSubmit={handleSubmit} className='bg-primarygray shadow-md rounded px-8 pt-6 pb-8 mb-4 w-3/4'>
+                    <div className='mb-4'>
+                        <label className='block text-white text-sm font-bold mb-2'>
+                        Email
+                        </label>
+                        <input className='shadow appearance-none border rounded w-full py-2 px-3 text-primarygray leading-tight focus:outline-none focus:shadow-outline' 
+                            id='email' 
+                            type='text' 
+                            placeholder='Email' 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
-                </div>
-            </form>
-        </div>
+                    <div className='mb-4'>
+                        <label className='block text-white text-sm font-bold mb-2'>
+                        Password
+                        </label>
+                        <input className='shadow appearance-none border rounded w-full py-2 px-3 text-primarygray mb-3 leading-tight focus:outline-none focus:shadow-outline' 
+                            id='password' 
+                            type='password' 
+                            placeholder='password' 
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className='md:flex md:items-center'>
+                        <div className='md:w-2/3'>
+                            <button className='shadow bg-primaryblue hover:bg-hoverblue focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded' type='submit'>
+                                {buttonText}
+                            </button>
+                            <a className='inline-block align-baseline font-bold text-sm text-primaryblue hover:text-hoverblue px-4' href={route}>
+                                {routeText}
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </>
     );
 }
 

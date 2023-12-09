e878be9c762df52c0422fa48ed2bc1d3c63ede2a
Michael Graessle <michael.graessle@gmail.com>
2023-12-08 16:58:19 -0800

    fixing padding and look of sidebar by removing rounded corners and box shadow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 
 const Home = () => {
     return (
-        <div className='bg-primary p-4 flex flex-row min-h-screen h-screen'>
+        <div className='bg-primary flex flex-row min-h-screen h-screen'>
             <Sidebar />
             <div className='flex flex-col w-full h-screen bg-primary items-center content-center'>
                 <h1 className='text-white'>Inbox</h1>

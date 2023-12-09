20304ae2595af8e78aff4e53fc6a226e5933adde
Michael Graessle <michael.graessle@gmail.com>
2023-12-08 16:55:12 -0800

    changing bg colors to match todoist dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      'primary': '#2d2d2d',
+      'primary': '#1e1e1e',
       'white': '#ffffff',
       'primarygray': '#454545',
       'primaryblue': '#0085ff',

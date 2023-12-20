221a1e95037a88e38a53c9660322ce69c1b0c5f3
Michael Graessle <michael.graessle@gmail.com>
2023-12-19 22:10:55 -0800

    fixing issue with parsing .env file for firebase config

---
+++ Api/config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
--- Api/src/Connections/FirebaseConnection.ts
+++ Api/src/Connections/FirebaseConnection.ts
@@ -1,21 +1,13 @@
 import { IConnection } from "./IConnection";
 import { initializeApp } from "firebase/app";
 import { collection, query, where, doc, addDoc, getDocs, deleteDoc, getFirestore, Firestore } from "firebase/firestore";
+import { firebaseConfig } from "../../config";
 
 export class FirebaseConnection<T> implements IConnection<T> {
     readonly db: Firestore;
     readonly collectionName: string;
 
     constructor(collectionName: string) {
-        const  firebaseConfig = {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID ,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.FIREBASE_APP_ID
-        };
-
         const firebaseapp = initializeApp(firebaseConfig);
         this.db = getFirestore(firebaseapp);
         this.collectionName = collectionName;

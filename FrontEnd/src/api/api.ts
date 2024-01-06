34da34c9e711ad10f99415f0c5a1826fe3e613a3
Michael Graessle <michael.graessle@gmail.com>
2024-01-05 18:29:26 -0800

    first pass at adding api calls to front end

---
+++ FrontEnd/src/api/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export interface Todo {
+    id: string;
+    task: string;
+    scheduledDate: string;
+    completed: boolean;
+    userId: string;
+}
+
+export default class Api {
+    static async getTodos(userId: string) : Promise<Todo[]> {
+        const apiUrl : string = import.meta.env.VITE_API_URL;
+        return (await axios.get(`${apiUrl}/todos?userId=${userId}`)).data;
+    }
+}

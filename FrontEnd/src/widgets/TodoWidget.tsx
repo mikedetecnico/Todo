57128e46e81b38ab711d25e5da8df1cb2d8b92bf
Michael Graessle <michael.graessle@gmail.com>
2024-02-05 16:34:09 -0800

    Refactor TodoWidget layout to justify centering

--- FrontEnd/src/widgets/TodoWidget.tsx
+++ FrontEnd/src/widgets/TodoWidget.tsx
@@ -10,14 +10,14 @@ interface TodoWidgetProps {
 const TodoWidget = ({todo, index, onDeleteTodo, onOpenEditModal}: TodoWidgetProps) => {
     return (
         <div key={`todo${index}`} className='flex flex-row w-full h-1/6 bg-primary p-6 hover:bg-hovergray'>
-            <div className='flex flex-col w-1/6 h-full'>
+            <div className='flex flex-col w-1/6 h-full justify-center'>
                 <input type='checkbox' className='rounded-lg bg-primary' placeholder='Complete' onClick={() => onDeleteTodo(todo.id)}/>
             </div>
             <div className='flex flex-row w-full h-full' onClick={() => onOpenEditModal(todo)}>
-                <div className='flex flex-col w-5/6 h-full text-white'>
+                <div className='flex flex-col w-5/6 h-full text-white justify-center'>
                     <h1>{todo.task}</h1>
                 </div>
-                <div className='flex flex-col w-5/6 h-full text-white'>
+                <div className='flex flex-col w-5/6 h-full text-white justify-center'>
                     <h1>{todo.scheduledDate}</h1>
                 </div>
             </div>

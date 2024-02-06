e528ad1a9f170cfacd57c33bd29c773fab2e48a3
Michael Graessle <michael.graessle@gmail.com>
2024-02-05 17:06:52 -0800

    adding button type and title to fix lint error

--- FrontEnd/src/components/MainTopHeader.tsx
+++ FrontEnd/src/components/MainTopHeader.tsx
@@ -23,7 +23,7 @@ const MainTopHeader = () => {
                 <span className="font-semibold text-xl tracking-tight">ToDo</span>
             </div>
             <div className="block lg:hidden">
-                <button onClick={() => {setShowLinks(!showLinks)}} className="flex items-center px-3 py-2 border rounded text-primaryblue border-teal-400 hover:text-hoverblue hover:border-white">
+                <button type='button' onClick={() => {setShowLinks(!showLinks)}} className="flex items-center px-3 py-2 border rounded text-primaryblue border-teal-400 hover:text-hoverblue hover:border-white" title="Toggle Menu">
                     <svg className="fill-white h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>

f357108bce846f06a70df4daec8edc74c9d1a361
Michael Graessle <michael.graessle@gmail.com>
2024-02-07 15:54:16 -0800

    adding route to login after signup successful

--- FrontEnd/src/features/auth/useSignup.ts
+++ FrontEnd/src/features/auth/useSignup.ts
@@ -1,6 +1,7 @@
 import { toast } from "react-hot-toast";
 import IAuth from "./IAuth";
 import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export interface IAuthParams {
     email: string;
@@ -10,12 +11,16 @@ export interface IAuthParams {
 }   
 
 export function useSignup(auth: IAuth) {
+    const navigate = useNavigate();
+    
     const {mutate: signup, isPending} = useMutation({
         mutationFn: async (params: IAuthParams) => { await auth.signUp(params.email, params.password, params.firstName, params.lastName); },
         onSuccess: () => {
             toast.success(
             "Account successfully created! Please verify the new account from the user's email address."
             );
+
+            navigate("/login", { replace: true });
         },
         onError: (error: Error) => {
             if (error.message.includes("email-already-in-use")) {

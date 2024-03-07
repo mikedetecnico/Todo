469360ca63d2d01d1293f1c3f19dd68cb115ebce
Michael Graessle <michael.graessle@gmail.com>
2024-03-06 17:47:22 -0800

    adding rate limiter for preventing brute force attacks

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -3,11 +3,19 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { VerifyToken } from './Middleware/VerifyToken';
 import todoRouter from './Router/TodoRouter';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(VerifyToken);
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
 app.use("/todos", todoRouter);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
